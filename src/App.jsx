ee7dc9ab195f05549e4961bce7e9ad6f22e1fdd4
chawingachimwemwe@gmail.com <chawingachimwemwe@gmail.com>
2024-10-21 08:06:54 +0200

    adding edit button

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 
 function App() {
-  
+
 
   return <main>
-   <div id="game-container">
-   <ol id="players">
-  <li> <span className="player-name">Player 1</span> 
-   <span className="player-symbol">X</span> </li>
+    <div id="game-container">
+      <ol id="players">
+        <li>
+          <span className="player">
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </span>
+          <button>Edit</button>
+        </li>
 
-   <li> <span className="player-name">Player 2</span> 
-   <span className="player-symbol">O</span> </li>
+        <li>
+          <span className="player">
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </span>
+          <button>Edit</button>
+
+        </li>
 
 
-   </ol>
+      </ol>
 
-   GAME BOARD
-   </div>
+      GAME BOARD
+    </div>
   </main>;
 }
 
